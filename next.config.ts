99e6c07f3a30806eeb9bc0deaaf785fc8f2c93cf
Gustavo <gustavoberengani@gmail.com>
2025-12-09 22:07:27 -0300

    Correção config Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Isso avisa o Next.js para não "quebrar" a biblioteca de IA ao subir para a Vercel
+  serverExternalPackages: ['@xenova/transformers'],
 };
 
 export default nextConfig;
